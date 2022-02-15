7b3c1a89ebe5c1190d246b260e1a06c70ee431fe
Jake Snyder <jak3legacy@gmail.com>
2022-02-15 09:51:46 -0500

    refined 2 and 3 with Mitch in review

--- script.js
+++ script.js
@@ -71,33 +71,37 @@
 
 
 // >>>>>>>>>> Modified V3 (For Loop; Using Array) <<<<<<<<<<
-const totalP = document.querySelector(".total");
-const buttons = document.querySelectorAll(".button");
-const itemsArray = [
-    {name: "LIME COLA", price: 2},
-    {name: "SALTED PEANUTS", price: 3},
-    {name: "CHOCOLATE BAR", price: 4},
-    {name: "FRUIT GUMMIES", price: 6}
-];
-let total = 0;
+// const totalP = document.querySelector(".total");
+// const buttons = document.querySelectorAll(".button");
+// const itemsArray = [
+//     {name: "LIME COLA", price: 2},
+//     {name: "SALTED PEANUTS", price: 3},
+//     {name: "CHOCOLATE BAR", price: 4},
+//     {name: "FRUIT GUMMIES", price: 6}
+// ];
+// let total = 0;
 
-for ( let i = 0 ; i <= itemsArray.length -1 ; i++ ) {
-    buttons[i].textContent = (`${itemsArray[i].name} $${itemsArray[i].price}`);
-    buttons[i].addEventListener("click", ()=> {
-        total += itemsArray[i].price;
-        totalP.textContent = `Total: $${total.toFixed(2)}`;
-    });
-};
+// for ( let i = 0 ; i <= itemsArray.length -1 ; i++ ) {
+//     buttons[i].textContent = (`${itemsArray[i].name} $${itemsArray[i].price}`);
+//     buttons[i].addEventListener("click", ()=> {
+//         total += itemsArray[i].price;
+//         totalP.textContent = `Total: $${total.toFixed(2)}`;
+//     });
+// };
 
 
 // // Question 2
+const totalParagraph = document.querySelector(".total")
+const vendingButtons = document.querySelectorAll(".two button")
 const form = document.querySelector(".two form");
+const bank = document.querySelector(".bank")
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const howMany = document.querySelector("#howMany").value;
     const whichCoin = document.querySelector("#whichCoin").value;
     for ( let i=0 ; i < howMany ; i++ ) {
-        const newCoin = document.createdElement("div");
+        const newCoin = document.createElement("div");
         newCoin.classList.add("coin", whichCoin);
         newCoin.textContent = whichCoin;
         bank.append(newCoin);
@@ -105,3 +109,28 @@ form.addEventListener("submit", (event) => {
 });
 
 // Question 3
+const bulb = document.querySelector(".bulb")
+const on = document.querySelector(".on-btn");
+const off = document.querySelector(".off-btn");
+const toggle = document.querySelector(".toggle-btn");
+const end = document.querySelector(".end-btn");
+
+on.addEventListener("click", ()=> {
+    bulb.classList.add("on");
+});
+
+off.addEventListener("click", ()=> {
+    bulb.classList.remove("on");
+});
+
+toggle.addEventListener("click",()=> {
+    bulb.classList.toggle("on");
+});
+
+end.addEventListener("click", ()=> {
+    bulb.remove();
+    on.disabled = true;
+    off.disabled = true;
+    toggle.disabled = true;
+    end.disabled = true;
+});
